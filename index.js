ac962eb92edbbf9824977d6c7c46b1153f9b4945
Max <max.edison@yahoo.com>
2021-10-12 16:48:55 -0400

    commit tree 077d135150d57bf55f00f952380746d2b99688bf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+let currentUser = "Slim Jim Phantom";
+// currentUser is defined
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// welcomeMessage contains "Welcome to Flatbook, " + contains the value of the 'currentUser' variable + ends with an exclamation point!
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// excitedWelcomeMessage contains "WELCOME TO FLATBOOK, " + contains the value of the 'currentUser' variable + ends with an exclamation point
+
+let shortGreeting = "Welcome, " + currentUser[0]+"!"
+//shortGreeting contains "Welcome, " contains the first initial of the name stored in the 'currentUser' variable + ends with an exclamation point
